6f8232666f6fb57c9f0a170651a1aadcdd321bb7
Brian Wheeler <bdwheele@indiana.edu>
2024-08-19 12:23:24 -0400

    loading and normalized are finished.

--- normalize_content_media.py
+++ normalize_content_media.py
@@ -10,78 +10,104 @@ from pathlib import Path
 import shutil
 import subprocess
 import yaml
-from utils import get_duration
+from utils import get_duration, human_time
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", default=False, action="store_true", help="Turn on debugging")
-    parser.add_argument("--channels", type=int, default=1, help="Number of audio channels")
-    parser.add_argument("--sample_rate", type=int, default=44100, help="Audio Sample Rate")
-    parser.add_argument("--sample_size", type=int, default=16, choices=[8, 16, 24, 32], help="Bits per sample")
-    parser.add_argument("--silence_threshold", type=int, default=50, help="Silence threshold (positive number in -dB)")
-    parser.add_argument("source", type=Path, help="Source root for the media files")
-    parser.add_argument("destination", type=Path, help="Destination root for the media files")
+    parser.add_argument("--silence_threshold", type=int, default=60, help="Silence threshold (positive number in -dB)")
+    parser.add_argument("--pop_length", type=float, default=0.2, help="Length of pops to be ignored")
+    parser.add_argument("--adaptive_gap", default=False, action="store_true", help="Use adaptive pop management")
+    parser.add_argument("--adaptive_factor", default=1.0, type=float, help="adaptive gap factor multiplier")
+    parser.add_argument("source", type=Path, help="Source for the media files")    
     args = parser.parse_args()
     logging.basicConfig(format="%(asctime)s [%(levelname)-8s] (%(filename)s:%(lineno)d:%(process)d)  %(message)s",
                         level=logging.DEBUG if args.debug else logging.INFO)
 
-    if not all([args.destination.is_dir(), args.source.is_dir()]):
+    if not args.source.is_dir():
         logging.error("The source and destination must be directories")
         exit(1)
     source: Path = args.source
-    dest: Path = args.destination
+    
 
     # write the normalization parameters to the destination directory
     # so we can reproduce it if needed.
-    with open(dest / "normalization_parameters.yaml", "w") as f:
-        yaml.safe_dump({'channels': args.channels,
-                        'sample_rate': args.sample_rate,
-                        'sample_size': args.sample_size,
-                        'silence_threshold': args.silence_threshold}, f)
+    with open(source / "normalization_parameters.yaml", "w") as f:
+        yaml.safe_dump({'silence_threshold': args.silence_threshold,
+                        'pop_length': args.pop_length,
+                        'adaptive_gap': args.adaptive_gap,
+                        'adaptive_factor': args.adaptive_factor}, f)
+    
     original_duration = 0
     new_duration = 0
 
-    for sdir in source.glob("*"):        
-        ddir = dest / sdir.name
-        logging.info(f"Processing {ddir}")
-        ddir.mkdir(exist_ok=True)
-        # copy any yaml files
-        for y in sdir.glob("*.yaml"):
-            shutil.copy(y, ddir)
+    for sdir in source.glob("*"):                
+        logging.info(f"Processing {sdir}")        
         # convert and strip the media files
         for mediafile in sdir.glob("*.mp4"):
-            infile = str(mediafile.resolve())
-            tmpfile = (ddir / mediafile.with_suffix(".tmp.wav").name).resolve()
-            tmpfile.parent.mkdir(exist_ok=True, parents=True)            
-            ffmpeg_command = ["ffmpeg", "-i", infile, "-y",                       
-                              "-c:a", f"pcm_s{args.sample_size}le",
-                              "-ac", str(args.channels),
-                              "-r:a", f"pcm_s{args.sample_rate}le",
-                              str(tmpfile)]
-            outfile = (ddir /mediafile.with_suffix(".wav").name).resolve()            
-            sox_command = ["sox", str(tmpfile), str(outfile),
-                           'reverse', 'silence', '1', '1', f'-{args.silence_threshold}dB', 'reverse']
-            
             try:
-                logging.debug(f"Ffmpeg Command: {ffmpeg_command}")
-                p = subprocess.run(ffmpeg_command, stdin=subprocess.DEVNULL,
-                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   check=True, encoding="utf-8")
-                logging.debug(f"SOX Command: {sox_command}")
-                p = subprocess.run(sox_command, stdin=subprocess.DEVNULL,
-                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   check=True, encoding="utf-8")
-                oduration = get_duration(mediafile)
-                original_duration += oduration
-                nduration = get_duration(outfile)
+                duration = get_duration(mediafile)
+                original_duration += duration
+                p = subprocess.run(["ffmpeg", "-i", str(mediafile.resolve()),
+                                    '-af', f'silencedetect=noise=-{args.silence_threshold}dB',
+                                    '-f', 'null', '-'],
+                                    stdin=subprocess.DEVNULL, stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT, encoding='utf-8',
+                                    check=True)
+                segments = []
+                for l in p.stdout.splitlines():
+                    if 'silence_duration' in l:
+                        parts = l.split()
+                        s = {'start': float(parts[4]) - float(parts[7]),
+                             'end': float(parts[4])}
+                        segments.append(s)
+                
+                nduration = duration
+                
+                
+                if args.adaptive_gap:
+                    for seg in reversed(segments):
+                        l = (seg['end'] - seg['start']) * args.adaptive_factor
+                        if nduration - seg['end'] < l:
+                            logging.debug(f"Setting duration to {seg['start']} because gap between segment and duration ({nduration - seg['end']}) is less than {l}")
+                            nduration = seg['start']
+                        else:
+                            break
+                else:
+                    for seg in reversed(segments):
+                        #print("Testing segment: ", seg)                    
+                        if nduration - args.pop_length < seg['end']:
+                            # there's a pop (or overlap) at the end..move the duration.
+                            #print(f"test point at {cpoint} is less than the end..")
+                            nduration = seg['start']
+                        else:
+                            #print("segment is out of bound")
+                            break
+                
+                
                 new_duration += nduration
-                logging.info(f"Normalizing {mediafile.name}: {oduration:0.3f} -> {nduration:0.3f}")
+                logging.debug(f"{duration}, {nduration}, {segments}")
+                                
+                tfile = mediafile.with_suffix(".tmp")
+                mediafile.rename(tfile)
+                p = subprocess.run(['ffmpeg', '-i', str(tfile.resolve()), '-y',
+                                    '-c:a', 'copy', '-c:v', 'copy', 
+                                    '-t', str(nduration), str(mediafile.resolve())],
+                                    stdin=subprocess.DEVNULL, stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT, check=True,
+                                    encoding='utf-8')
+                tfile.unlink(missing_ok=True)
+                # write the normalization information
+                with open(mediafile.parent / f"{mediafile.name}.normalization.yaml", "w") as f:
+                    yaml.safe_dump({'filename': str(mediafile),
+                                    'original_duration': duration,
+                                    'truncated_duration': nduration}, f)
+                logging.info(f"Removed trailing silence:  {human_time(duration)} -> {human_time(nduration)} (reduction: {human_time(duration - nduration)})")
+
             except Exception as e:
                 logging.exception(f"Cannot transcode {mediafile}: {e}")
-            finally:
-                tmpfile.unlink(missing_ok=True)
 
-    logging.info(f"Total duration of normalized data: {new_duration:0.3f}, {original_duration-new_duration:0.3f} seconds less than original")
+    logging.info(f"Total duration of normalized data: {human_time(new_duration)}, {human_time(original_duration-new_duration)} less than original")
 
 
 if __name__ == "__main__":
